810d802746456ed486f815e3eabc248f6856e4fa
Mia.Zhang <mia7zwy@hotmail.com>
2022-09-01 20:37:35 +1200

    chore: structure test

---
+++ src/components/Card.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Card = () => {
+  return <div className="card">card test</div>;
+};
+
+export default Card;
---
+++ src/components/CartItem.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CartItem = () => {
+  return <div className="cartItem">cart Itemss</div>;
+};
+
+export default CartItem;
